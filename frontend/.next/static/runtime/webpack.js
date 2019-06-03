47af3797c29b8b5a8a2f77eee8fb2b75aaf21454
Max Euler <maxjuliuseuler@gmail.com>
2019-06-03 15:16:03 +0200

    add costum app and header

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "075f78239b69470707d5";
+/******/ 	var hotCurrentHash = "9414bc4220fc06a9b9ad";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ frontend/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			<header>
+				<p>Logo</p>
+				<p>Account</p>
+			</header>
+		);
+	}
+}
+
+export default Header;
