379b6599fa80fa1a18400d1f8d24ece152b4a160
Max Euler <maxjuliuseuler@gmail.com>
2019-06-06 18:18:33 +0200

    new setup

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1ab35cd43fdde875da12";
+/******/ 	var hotCurrentHash = "932203b3aa1915acf0cd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
