df8f10735cdb4eaaa9407e326df2f6dfdb88fef2
Max Euler <maxjuliuseuler@gmail.com>
2019-06-06 20:04:24 +0200

    mongo db setup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+require('dotenv').config({ path: 'variables.env' });
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', err => {
+	console.log(`🚫  🙅  🚫  🙅  🚫  👉  ${err.message}`);
+});
 
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
